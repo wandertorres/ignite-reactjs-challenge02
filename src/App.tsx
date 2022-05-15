0279534ee56878481cc55c4479aa2175aa40d183
Wander Torres <wanderats@gmail.com>
2022-05-15 19:28:27 -0300

    implements SideBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
-import { api } from './services/api';
-
-import { Button } from './components/Button';
 import { Content } from './components/Content';
+import { SideBar } from './components/SideBar';
 // import { SideBar } from './components/SideBar';
 // import { Content } from './components/Content';
 
@@ -11,55 +9,20 @@ import './styles/global.scss';
 import './styles/sidebar.scss';
 import './styles/content.scss';
 
-interface GenreResponseProps {
-  id: number;
-  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
-  title: string;
-}
-
 export function App() {
   const [selectedGenreId, setSelectedGenreId] = useState(1);
-  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
-  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
-
-  useEffect(() => {
-    api.get<GenreResponseProps[]>('genres').then(response => {
-      setGenres(response.data);
-    });
-  }, []);
-
-  useEffect(() => {
-    api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
-      setSelectedGenre(response.data);
-    })
-  }, [selectedGenreId]);
-
-  function handleClickButton(id: number) {
-    setSelectedGenreId(id);
-  }
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
-      <nav className="sidebar">
-        <span>Watch<p>Me</p></span>
-
-        <div className="buttons-container">
-          {genres.map(genre => (
-            <Button
-              key={String(genre.id)}
-              title={genre.title}
-              iconName={genre.name}
-              onClick={() => handleClickButton(genre.id)}
-              selected={selectedGenreId === genre.id}
-            />
-          ))}
-        </div>
-      </nav>
-
-      <Content 
-        titleSelectedGenre={selectedGenre.title} 
+      <SideBar 
+        setSelectedGenreId={setSelectedGenreId} 
         selectedGenreId={selectedGenreId} 
       />
+
+      {/*<Content 
+        titleSelectedGenre={selectedGenre.title} 
+        selectedGenreId={selectedGenreId} 
+  />*/}
     </div>
   )
 }
